6cc088e17a618c107ca8466df7ca55557530e812
Sebastian Ślęzak <41953849+SebastianSlezak@users.noreply.github.com>
2024-12-06 00:46:00 +0100

    lab2

---
+++ lab2/README.md
@@ -0,0 +1,7 @@
+### Wyniki z konsoli
+<hr>
+
+```
+Color[red=12, green=15, blue=10, alpha=0]
+Color[red=12, green=15, blue=10, alpha=50]
+```
---
+++ lab2/src/main/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Color color1 = new Color(12, 15, 10);
+        Color color2 = new Color(12, 15, 10, 50);
+        
+        System.out.println(color1);
+        System.out.println(color2);
+    }
+}
